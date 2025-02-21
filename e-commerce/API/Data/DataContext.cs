ddc286e098a3c407ee381a80a6cc5a7bc89fac3e
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-02-21 10:19:09 +0300

    fullstack-eticaret-web
    Developed with .NET9 and React

---
+++ e-commerce/API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
