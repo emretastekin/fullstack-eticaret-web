04ee1818311d4159254d865824bdbfe5634b92db
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-02-22 19:26:49 +0300

    react state konusu öğrenildi.

--- e-commerce/Client/src/App.tsx
+++ e-commerce/Client/src/App.tsx
@@ -1,8 +1,5 @@
-const products = [
-  { id:1 ,name:"product1", price:"1000", is_active: true },
-  { id:2 ,name:"product2", price:"2000", is_active: false },
-  { id:3 ,name:"product3", price:"3000", is_active: true },
-];
+import { useState } from "react";
+
 
 function App() {
   return (
@@ -20,6 +17,17 @@ function Header() {
 }
 
 function ProductList() {
+
+  const [products, setProducts] = useState([
+    { id:1 ,name:"product1", price:"1000", is_active: true },
+    { id:2 ,name:"product2", price:"2000", is_active: false },
+    { id:3 ,name:"product3", price:"3000", is_active: true },
+  ]);
+
+  function addProduct(){
+    setProducts([...products, { id:Date.now() ,name:"product4", price:"4000", is_active: true }])
+  }
+
   return (
     <div>
       <h2>ProductList</h2>
@@ -27,6 +35,9 @@ function ProductList() {
       { products.map(p => (
         <Product key={p.id} product={p} />
       ))}
+
+      <button onClick={addProduct}>Add Product</button>
+
     </div>
   );
 }
