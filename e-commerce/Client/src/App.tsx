06a84c7401282256b3560cb84b98dc5b8035bc33
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-02-22 12:03:12 +0300

    JSX Lists kısmı öğrenildi.

--- e-commerce/Client/src/App.tsx
+++ e-commerce/Client/src/App.tsx
@@ -1,7 +1,7 @@
 const products = [
-  { name:"product1", price:"1000" },
-  { name:"product2", price:"2000" },
-  { name:"product3", price:"3000" },
+  { id:1 ,name:"product1", price:"1000", is_active: true },
+  { id:2 ,name:"product2", price:"2000", is_active: false },
+  { id:3 ,name:"product3", price:"3000", is_active: true },
 ];
 
 function App() {
@@ -23,19 +23,24 @@ function ProductList() {
   return (
     <div>
       <h2>ProductList</h2>
-      <Product product={products[0]} />
-      <Product product={products[1]}/>
-      <Product product={products[2]}/>
+
+      { products.map(p => (
+        <Product key={p.id} product={p} />
+      ))}
     </div>
   );
 }
 
 function Product(props: any) {
   return(
-    <div>
-      <h3>{props.product.name}</h3>
-      <p>{props.product.price}</p>
+    <>
+    { props.product.is_active ? (
+      <div>
+        <h3>{props.product.name}</h3>
+        <p>{props.product.price}</p>
     </div>
+    ) : <p>Ürün satışta değil.</p>}
+    </>
   );
 }
 
