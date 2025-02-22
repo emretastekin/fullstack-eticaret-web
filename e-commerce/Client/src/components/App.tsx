b160c8723ac2398d1d1562a157ca6f460d635538
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-02-22 21:19:12 +0300

    componets klasörü oluşturuldu. tüm componentler için bu klasör altında dosyalar oluşturuldu. tyscript sayesinde daha güvenli ve parçalı bir kod yazımı oldu.

---
+++ e-commerce/Client/src/components/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { IProduct } from "../model/IProduct";
+import Header from "./Header";
+import ProductList from "./ProductList";
+
+
+function App() {
+
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+    .then(response => response.json())
+    .then(data => setProducts(data));
+  }, [])
+
+
+  function addProduct(){
+    setProducts([...products, 
+      { 
+        id:Date.now() ,
+        name:"product4", 
+        price:4000, 
+        isActive: true 
+      }])
+  }
+
+  return (
+    <>
+      <Header products={products} />
+      <ProductList products={products} addProduct={addProduct} />
+    </>
+  )
+}
+
+
+export default App
