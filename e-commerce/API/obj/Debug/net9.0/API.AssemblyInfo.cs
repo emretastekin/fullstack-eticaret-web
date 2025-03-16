df9537f9e26123c6125e519dedc8e93030ef3c5d
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-03-16 21:54:53 +0300

    redux ve redux toolkit kütüphaneleri kuruldu.

--- e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f812cbaabd1a0c38c46c90dbedeb079b5010f42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb3eaf8e88004fb79a4a290e0d9cdfd15393ae38")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
