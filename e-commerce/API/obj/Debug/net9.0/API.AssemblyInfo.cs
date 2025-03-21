2d8fc2688b20faebec934a5e8ea75ce4ed532683
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-03-21 17:11:44 +0300

    createAsyncThunk fonksiyonu kullanılarak; cartSlice içerisinde add ve delete metodları oluşturuldu.

--- e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9600de6a5f69da15dc42457661a1a41fffdb0c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa6ca207c980ebd37965f5ff90870f5e17aced5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
