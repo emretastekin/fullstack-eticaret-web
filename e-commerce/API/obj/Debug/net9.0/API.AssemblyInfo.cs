8eb129bf146c858dbdba2fdc58197496315f8ea6
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-04-04 23:38:58 +0300

    .commit

--- e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50b199f017ffc5c369a9888d7bf8e73d23334b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b46114ea73b8930e14d03c3de939d9669fdd1c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
