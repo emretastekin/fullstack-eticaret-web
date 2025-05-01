468ee89afa5fef13b617d4905f425c67358e3075
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-05-01 20:45:59 +0300

    stepper componenti kullanılarak sayfa içerisindeki geçişler sağlandı.

--- e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08731461e4848264d3913747f70d6e7690285037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d45daa6e725e48180872dba9ba95d5553d64270")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- e-commerce/Client/src/features/checkout/CheckoutPage.tsx
+++ e-commerce/Client/src/features/checkout/CheckoutPage.tsx
@@ -1,23 +1,92 @@
-import { Info } from "@mui/icons-material";
-import { Grid2, Paper } from "@mui/material";
+import { ChevronLeftRounded, ChevronRightRounded, Info } from "@mui/icons-material";
+import { Box, Button, Grid2, Paper, Step, StepLabel, Stepper } from "@mui/material";
 import AddressForm from "./AddressForm";
 import PaymentForm from "./PaymentForm";
 import Review from "./Review";
 import InfoForm from "./Infoo";
 import Infoo from "./Infoo";
+import { useState } from "react";
+
+
+
+const steps = ["Teslimat Bilgileri", "Ödeme", "Sipariş"];
+
+function getStepContent(step: number)
+{
+    switch(step){
+        case 0:
+            return <AddressForm />;
+        case 1:
+            return <PaymentForm />;
+        case 2:
+            return <Review />;
+        default:
+            throw new Error("Bilinmeyen bir step");
+    }
+}
+
 
 export default function CheckoutPage()
 {
+
+    const [activeStep, setActiveStep] = useState(0);
+
+    function handleNext(){
+        setActiveStep(activeStep + 1);
+    }
+
+    function handlePrevious(){
+        setActiveStep(activeStep -1);
+    }
+
     return (
         <Paper>
-            <Grid2 container sx={{p:4}} spacing={2}>
+            <Grid2 container sx={{p:4}} spacing={4}>
                 <Grid2 size={4}>
                     <Infoo />
                 </Grid2>
                 <Grid2 size={8}>
-                    <AddressForm />
-                    <PaymentForm />
-                    <Review />
+                    <Box>
+                        <Stepper activeStep={activeStep} sx={{height: 40}} >
+                            { steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                    </Box>
+                    <Box>
+                        {activeStep === steps.length ? (
+                            <h2>Sipariş Tamamlandı</h2>
+                        ) : (
+                            <>
+                            {getStepContent(activeStep)}
+                            <Box>
+                                <Box sx={
+                                    [
+                                        {
+                                            display: "flex",
+                                        },
+                                        activeStep !== 0
+                                            ? {justifyContent: "space-between"}
+                                            : {justifyContent: "flex-end"}
+                                    ]
+                                }>
+                                    {
+                                        activeStep !== 0 &&
+                                            <Button startIcon={<ChevronLeftRounded />} variant="contained" 
+                                            onClick={handlePrevious}>Geri</Button>
+                                    }
+
+                                    
+                                    <Button startIcon={<ChevronRightRounded />} variant="contained" 
+                                        onClick={handleNext}>İleri</Button>
+                                </Box>
+                            </Box>
+                            </>
+                        )}
+                        
+                    </Box>
                 </Grid2>
             </Grid2>
         </Paper>
