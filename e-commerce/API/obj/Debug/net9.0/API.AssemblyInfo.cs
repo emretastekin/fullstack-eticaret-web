b629fb94e7ea1e02c4c351585be88e0f390ddc63
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-05-06 17:30:29 +0300

    orderList sayfası tamamlandı.

--- e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388b5d352460ea528266a211d122d6fefa9e6757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd563449e9838fb9d1ccaabe6b5ceba9d93fcf6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
